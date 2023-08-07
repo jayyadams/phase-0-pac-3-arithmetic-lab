f8eee496e08ad68a825fef8bd9e8dd34f71f1b18
Jordan Adams <jcadamsboomer@gmail.com>
2023-08-07 16:56:09 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+var n = 0;
+function increment(n){
+
+  n++;
+  return n;
+}
+
+var n = 10;
+function decrement(n){
+
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+    parseInt("32");
+    return n;
+}
+
+function makeInt(n) {
+    return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
